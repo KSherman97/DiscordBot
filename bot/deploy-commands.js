8d62a8e1498f9cb64b436991f23056f224103940
Kyle S <ksherm337@gmail.com>
2021-09-25 19:47:56 -0400

    coded the bot :(
    wrote the damn bot

---
+++ bot/deploy-commands.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId, token } = require('./auth.json');
+
+const commands = [
+	new SlashCommandBuilder().setName('lyric').setDescription('Replies with random lyric!')
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
