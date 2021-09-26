bbd88dd1426f54da54a35e73eb81c9feba0841f6
Kyle S <ksherm337@gmail.com>
2021-09-26 18:03:49 -0400

    wrote file parser

--- bot/index.js
+++ bot/index.js
@@ -1,6 +1,6 @@
 /**
  * Discord Bot
- * created by Kyle Sherman
+ * created by Ksherm337
  * project started on 09/25/2021
  */
 
@@ -11,90 +11,41 @@
  */
 
 // string array of the lyrics
-const lyricLinesArray = [
-    "Father of lies",
-    "cum in disguise",
-    "Your cum won't last",
-    "There's a snake in my ass",
-    "The cum-fathers secret stash",
-    "cum-stomp me flat",
-    "I'm going to fuck your dad",
-    "cumming high into the morning sky",
-    "Vape cum from my bum til I die",
-    "Watching Arthur with a cock in my ass",
-    "Riding hard",
-    "Eating ass master-class",
-    "Sacred cum blade",
-    "A fuckling crusade",
-    "Fatal cum--theft",
-    "Give me cum or give me death",
-    "Elon's Musk",
-    "Jesus Crust",
-    "Stealing donations from the cum-Czar's trust",
-    "A cum smoothie gulped smoothly",
-    "Consume the cum chalice",
-    "fuck everyone named Alex",
-    "David Hayter",
-    "cum Crusador",
-    "The Holy cum Wars",
-    "Razor-blade Masturbator",
-    "Margret Thatcher",
-    "The cum Snatcher",
-    "Father drowned",
-    "Going down on the cum clown",
-    "Prolapse pounding",
-    "Toothpick sounding",
-    "cum baking",
-    "My nipple-pussy is aching",
-    "cum fooler",
-    "Semen drooler",
-    "Forbidden cum-spice",
-    "Your shit-box looks nice!",
-    "Life is a cage, and death is the key",
-    "All your cum are belong to me",
-    "Normalize crying over spilt cum",
-    "Making cum-angels with my son",
-    "I fucked a fairy in half",
-    "How many holes does a human have?",
-    "My butt and cunny are in agony",
-    "Castration in the sky",
-    "Your penis will fly",
-    "Scrotal chambers",
-    "Semen sailors",
-    "Mommy's cum tax",
-    "Grind my balls on an axe!",
-    "cum-scented candle",
-    "cum-broiled eggs",
-    "cum-Christ consciousness",
-    "Third-eye, cum spy",
-    "cum-scrote sailboat",
-    "Semen speed racer",
-    "Off-road cum chode",
-    "My uterus came out!",
-    "cum treasurer",
-    "dick measurer",
-    "Irresponsible Manager of cum",
-    "A cum-slave, back from the grave",
-    "The price for breaking a cum-oath",
-    "James Hector",
-    "cum key-lector (or collector, vote in comments)",
-    "I tripped in the cum-keeper's crypt",
-    "cum feeder",
-    "Moist meter",
-    "Sans Undertale, the cum reaper",
-    "fucking a skeleton, right in the pussy",
-    "The Dark Souls of cum",
-    "cum-framed, and cum-blamed",
-    "cum-drowning awareness day",
-    "Brewing cum-fuel after school",
-    "Your nipples are crunchy",
-    "The tragic cum-sponge",
-    "Your cum is fading...",
-    "Sweep up the cum flakes, Joan"
-];
+// we will leave this empty for now
+// we will fill it by parsing a text file with \n delimiter
+var lyricLinesArray = [];
 
-// get the number of items in the lyrics array
-const lyricsLinesCount = lyricLinesArray.length;
+// file processing stuff
+// file reading documentation:
+//  https://nodejs.org/en/knowledge/file-system/how-to-read-files-in-nodejs/
+const fs = require('fs');
+const readline = require('readline');
+
+// define the file we want to read here
+// uses relative file path
+const lyricsFile = "lyrics.txt";
+
+// function structure
+// this creates a filestream with read capabilities
+function processLineByLine() {
+    var array = fs.readFileSync(lyricsFile).toString().split('\n');
+
+    for (i in array) {
+        lyricLinesArray[i] = array[i];
+    }
+}
+
+// use for testing
+function printLyrics() {
+
+    console.log(lyricsLinesCount);
+
+    var i = 0;
+    while (i < lyricsLinesCount){
+        console.log("line " + i + " " + lyricLinesArray[i]);
+        i++;
+    }
+}
 
 // gets random number from zero to max-1
 function getRandomNumber(max) {
@@ -109,6 +60,15 @@ function returnLyric() {
 
     return lyric;
 }
+
+// read the file here
+processLineByLine();
+
+// get the number of items in the lyrics array
+var lyricsLinesCount = lyricLinesArray.length;
+
+// use for testing
+// printLyrics();
 
 /**
  * ======================================
@@ -139,5 +99,5 @@ client.on('interactionCreate', async interaction => {
 });
 
 // log into discord with the client's token
+// comment out for testing purposes
 client.login(token);
-
